8a46dd658c719ede5d8387663124a9bafa4f8748
Marta Españó López <65234937+Martaesplo@users.noreply.github.com>
2023-06-15 12:53:07 +0200

    Add "part_1" to file names that require it.

--- fetch.sh
+++ fetch.sh
@@ -12,6 +12,11 @@ done
 for lan in "eo" "la" "sw"; do
 echo "Downloading $lan..."
 git lfs pull --include packaged/"$lan"/"$lan".txt.gz
+mv packaged/"$lan"/"$lan".txt.gz packaged/"$lan"/"$lan"_part_1.txt.gz
 done
 
-find ./packaged -mindepth 1 -type d ! -path './packaged/en' -type d ! -path './packaged/ru' -type d ! -path './packaged/el' -type d ! -path './packaged/de' -type d ! -path './packaged/pl' -type d ! -path './packaged/la' -type d ! -path './packaged/fr' -type d ! -path './packaged/zh' -type d ! -path './packaged/sw' -type d ! -path './packaged/ar' -type d ! -path './packaged/hi' ! -path './packaged/eo' -exec rm -rf {} +
+find ./packaged -mindepth 1 -type d ! -path './packaged/en' -type d ! -path './packaged/ru' \
+-type d ! -path './packaged/el' -type d ! -path './packaged/de' -type d ! -path './packaged/pl' \
+-type d ! -path './packaged/la' -type d ! -path './packaged/fr' -type d ! -path './packaged/zh' \
+-type d ! -path './packaged/sw' -type d ! -path './packaged/ar' \
+-type d ! -path './packaged/hi' ! -path './packaged/eo' -exec rm -rf {} +
