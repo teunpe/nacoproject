0ad6afe1633c6e2011a805a7d34e50745eeced68
Marta Españó López <marta.espano@alum.esci.upf.edu>
2023-06-25 18:35:33 +0200

    added r value to .R file and res25_1.txt

--- auc.R
+++ auc.R
@@ -14,7 +14,7 @@ getwd()
 #write into file
 #IMPORTANT: change 'run' variable every time the following chunks of code are run
 #for each time ALL the code chunks are ran: run = run + 1
-run=2
+run=1
 
 day<-format(Sys.Date(), "%d")
 filename <- sprintf("/res%s_%s.txt", day, run)
@@ -33,81 +33,93 @@ langs <- list('ar', 'de', 'el', 'en', 'eo', 'fr', 'hi', 'la', 'pl', 'ru', 'sw')
 
 write("LITERAL GLOBAL ALPHABET", res) 
 for(lang in langs) {
-  aucs <- list()
-  for(r in 0:2) {
-    score = readLines(sprintf("eo_%s.txt", r))
-    data_e = as.data.frame(score)
-    data_e$label <- 0
-    score = readLines(sprintf("%s_%s.txt", lang, r))
-    data_t = as.data.frame(score)
-    data_t$label <- 1
-    data_t = rbind(data_e, data_t)
-    auc = auc(roc(data_t$score, factor(data_t$label)))
-    print(lang)
-    print(auc)
-    aucs <- c(aucs, auc)
+  for(r in 3:5) {
+    aucs <- list()
+    write(sprintf("r=%s",r), res)
+    for (re in 0:2) {
+      score = readLines(sprintf("eo_%s_%s.txt", r, re))
+      data_e = as.data.frame(score)
+      data_e$label <- 0
+      score = readLines(sprintf("%s_%s_%s.txt", lang, r, re))
+      data_t = as.data.frame(score)
+      data_t$label <- 1
+      data_t = rbind(data_e, data_t)
+      auc = auc(roc(data_t$score, factor(data_t$label)))
+      print(lang)
+      print(auc)
+      aucs <- c(aucs, auc)
+    }
+    writeresults(lang, aucs)
   }
-  writeresults(lang, aucs)
 }
 
 #literal text, specific alphabet
 write("\nLITERAL SPECIFIC ALPHABET", res) 
 for(lang in langs) {
-  aucs <- list()
-  for(r in 0:2) {
-    score = readLines(sprintf("eo_spec_%s.txt", r))
-    data_e = as.data.frame(score)
-    data_e$label <- 0
-    score = readLines(sprintf("%s_spec_%s.txt", lang, r))
-    data_t = as.data.frame(score)
-    data_t$label <- 1
-    data_t = rbind(data_e, data_t)
-    auc = auc(roc(data_t$score, factor(data_t$label)))
-    print(lang)
-    print(auc)
-    aucs <- c(aucs, auc)
+  for(r in 3:5) {
+    aucs <- list()
+    write(sprintf("r=%s",r), res)
+    for(re in 0:2) {
+      score = readLines(sprintf("eo_%s_spec_%s.txt", r, re))
+      data_e = as.data.frame(score)
+      data_e$label <- 0
+      score = readLines(sprintf("%s_%s_spec_%s.txt", lang, r, re))
+      data_t = as.data.frame(score)
+      data_t$label <- 1
+      data_t = rbind(data_e, data_t)
+      auc = auc(roc(data_t$score, factor(data_t$label)))
+      print(lang)
+      print(auc)
+      aucs <- c(aucs, auc)
+    }
+    writeresults(lang, aucs)
   }
-  writeresults(lang, aucs)
 }
 
 #transliteral text, global alphabet
 write("\nTRANSLITERAL GLOBAL ALPHABET", res) 
 langs <- list('ar', 'el', 'hi', 'ru')
 for(lang in langs) {
-  aucs <- list()
-  for(r in 0:2) {
-    score = readLines(sprintf("eo_%s.txt", r))
-    data_e = as.data.frame(score)
-    data_e$label <- 0
-    score = readLines(sprintf("%s_transl_%s.txt", lang, r))
-    data_t = as.data.frame(score)
-    data_t$label <- 1
-    data_t = rbind(data_e, data_t)
-    auc = auc(roc(data_t$score, factor(data_t$label)))
-    print(lang)
-    print(auc)
-    aucs <- c(aucs, auc)
+  for(r in 3:5) {
+    aucs <- list()
+    write(sprintf("r=%s",r), res)
+    for(re in 0:2) {
+      score = readLines(sprintf("eo_%s_%s.txt", r, re))
+      data_e = as.data.frame(score)
+      data_e$label <- 0
+      score = readLines(sprintf("%s_%s_transl_%s.txt", lang, r, re))
+      data_t = as.data.frame(score)
+      data_t$label <- 1
+      data_t = rbind(data_e, data_t)
+      auc = auc(roc(data_t$score, factor(data_t$label)))
+      print(lang)
+      print(auc)
+      aucs <- c(aucs, auc)
+    }
+    writeresults(lang, aucs)
   }
-  writeresults(lang, aucs)
 }
 
 #transliteral text, specific alphabet
 write("\nTRANSLITERAL SPECIFIC ALPHABET", res) 
 for(lang in langs) {
-  aucs <- list()
-  for(r in 0:2) {
-    score = readLines(sprintf("eo_spec_%s.txt", r))
-    data_e = as.data.frame(score)
-    data_e$label <- 0
-    score = readLines(sprintf("%s_transl_spec_%s.txt", lang, r))
-    data_t = as.data.frame(score)
-    data_t$label <- 1
-    data_t = rbind(data_e, data_t)
-    auc = auc(roc(data_t$score, factor(data_t$label)))
-    print(lang)
-    print(auc)
-    aucs <- c(aucs, auc)
+  for(r in 3:5) {
+    aucs <- list()
+    write(sprintf("r=%s",r), res)
+    for(re in 0:2) {
+      score = readLines(sprintf("eo_%s_spec_%s.txt", r, re))
+      data_e = as.data.frame(score)
+      data_e$label <- 0
+      score = readLines(sprintf("%s_%s_transl_spec_%s.txt", lang, r, re))
+      data_t = as.data.frame(score)
+      data_t$label <- 1
+      data_t = rbind(data_e, data_t)
+      auc = auc(roc(data_t$score, factor(data_t$label)))
+      print(lang)
+      print(auc)
+      aucs <- c(aucs, auc)
+    }
+    writeresults(lang, aucs)
   }
-  writeresults(lang, aucs)
 }
 
